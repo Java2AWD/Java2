63d6918230697d331b66237eb90d3bf736ddb690
yasmans <yasmans@gmail.com>
2013-02-21 22:22:49 +0200

    Added test junit test :)

---
+++ test/dao/JdbcDAOMySqlTest.java
@@ -0,0 +1,12 @@
+package dao;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class JdbcDAOMySqlTest {
+	@Test
+	public void getConnectionTest () {
+		Assert.assertFalse(true);
+	}
+}
